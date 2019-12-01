90586167fa18e9c0a53fe2efedd4a94537abcc52
hopshackle <james@janigo.co.uk>
2019-12-01 15:35:14 +0000

    Added specificSettings for Asteroids Exhaustive Search

--- src/olderGames/asteroids/AsteroidsExhaustiveResult.kt
+++ src/olderGames/asteroids/AsteroidsExhaustiveResult.kt
@@ -4,29 +4,42 @@ import evodef.*
 import ggi.*
 import groundWar.*
 import groundWar.executables.*
+import java.io.BufferedReader
+import java.io.FileReader
 import java.util.*
+import kotlin.streams.toList
 
 fun main(args: Array<String>) {
     val sSpace = RHEASimpleSearchSpace(agentParamsFromCommandLine(args, "baseAgent", default = defaultRHEAAgent),
             fileName = args[0])
-    AsteroidsExhaustiveResult(sSpace, args[1].toInt())
+    val fileName = args.firstOrNull { it.startsWith("settingsFile") }?.split("=")?.get(1)
+    val settings = if (fileName == null) {
+        throw AssertionError("No data found for $fileName")
+    } else {
+        val fileAsLines = BufferedReader(FileReader(fileName)).lines().toList()
+        fileAsLines.map { it.split(",").map { v -> v.trim().toInt() } }
+    }
+    AsteroidsExhaustiveResult(sSpace, args[1].toInt(), settings)
 }
 
 class AsteroidsExhaustiveResult(val searchSpace: HopshackleSearchSpace<SimplePlayerInterface>,
-                                val maxGames: Int
+                                val maxGames: Int,
+                                val specificSettingsToTry: List<List<Int>> = emptyList()
+
 ) {
 
 
     // We run through every possible setting in the searchSpace, and run maxGames, logging the average result
-    val stateSpaceSize = (0 until searchSpace.nDims()).fold(1, { acc, i -> acc * searchSpace.nValues(i) })
-
     val allRanges = (0 until searchSpace.nDims()).map { 0 until searchSpace.nValues(it) }
 
     fun addDimension(start: List<List<Int>>, next: IntRange): List<List<Int>> {
         return start.flatMap { list -> next.map { list + it } }
     }
 
-    val allOptions = allRanges.fold(listOf(emptyList<Int>()), { acc, r -> addDimension(acc, r) })
+    val allOptions = if (specificSettingsToTry.isEmpty())
+        allRanges.fold(listOf(emptyList<Int>()), { acc, r -> addDimension(acc, r) })
+    else
+        specificSettingsToTry
 
     val optionScores = allOptions.map {
         val evaluator = AsteroidsEvaluator(searchSpace, EvolutionLogger())
